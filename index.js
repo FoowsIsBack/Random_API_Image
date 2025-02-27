9368ac5734a1ee646da44d9263142820485aea48
Foows - (Dave) <dbangcoyo11@gmail.com>
2025-02-27 21:40:35 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function cat() {
+    window.location.href = 'CAT_api/cat.html';
+}
+
+function dog() {
+    window.location.href = 'DOG_api/dog.html';
+}
+
+function quotes() {
+    window.location.href = 'QUOTES_api/quotes.html';
+}
